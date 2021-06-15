1322327511ba135f2e466c9e277f280a129fdf40
Paolo Matchett <paolomatchett@gmail.com>
2021-06-15 14:10:16 -0500

    Initial working Monday API query using itemId passed from webhook

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
 //const Shopify = require("shopify-api-node");
+const fetch = require("node-fetch");
 
 exports.handler = async (event) => {
-	let body = {};
-	let response = {};
-	let recievedData = {};
+  let body = {};
+  let response = {};
+  let recievedData = {};
+  let itemId;
 
-	//this is used to connect to a Monday.com Webhook integration and only necessary to run on first setting up
-	// https://support.monday.com/hc/en-us/articles/360003540679-Webhook-Integration-
-	/*
+  //this is used to connect to a Monday.com Webhook integration and only necessary to run on first setting up
+  // https://support.monday.com/hc/en-us/articles/360003540679-Webhook-Integration-
+  /*
 	if (event.body !== null && event.body !== undefined) {
 		body = JSON.parse(event.body);
 	}
 	if (event.challenge) body.challenge = event.challenge;
-	
-	//change groupName depending on what group EW has results inside
-	//todo: move Shopify request inside if statement to only run when form results are sent with appropriate data
-		if (event.body !== null && event.body !== undefined) {
-		body = JSON.parse(event.body);
-	}
-*/
-	if (event.event !== null && event.event !== undefined) {
-		console.info("EVENT\n" + JSON.stringify(event.event, null, 2));
-		recievedData = event.event;
-		if (recievedData.value.label.text == "APPROVED") {
-			body.approved = "yes, it was approved";
-			body.item = recievedData.pulseId;
-		}
-	}
+  */
 
-	/*
+  if (event.event !== null && event.event !== undefined) {
+    console.info("EVENT\n" + JSON.stringify(event.event, null, 2));
+    recievedData = event.event;
+    if (recievedData.value.label.text == "APPROVED") {
+      itemId = recievedData.pulseId;
+
+      //construct GraphQL query to get the entire Monday.com item data, since the webhook only sends us one column's value initially
+      let query =
+        "query items($id: Int) { items(ids: [$id] ) { id name column_values { id title value } } }";
+
+      fetch("https://api.monday.com/v2", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: process.env.MONDAY_API,
+        },
+        body: JSON.stringify({
+          query: query,
+          variables: { id: itemId },
+        }),
+      })
+        .then((r) => r.json())
+        //.then(function (data) {
+        //   recievedData = data;
+        //});
+        .then((data) => console.log("data returned:", JSON.stringify(data)));
+    }
+  }
+
+  /*
 	if (event?.event?.groupName == "NEW CUSTOMERS NEED SHOPIFY ACTIVATION") {
 		body.event = event.event;
 	}
 	*/
-	/*
+  /*
 	const shopify = new Shopify({
 		shopName: process.env.shopName,
 		apiKey: process.env.APIKey,
@@ -61,22 +78,27 @@ exports.handler = async (event) => {
 			send_email_invite: true,
 	}
 	*/
-	/*
+  /*
 	await shopify.customer
 		.create(newCustomer)
 		.then((customer) => console.info(customer))
 		.catch((error) => console.info(error));
 	*/
 
-	response = {
-		statusCode: 200,
-		body: JSON.stringify(body),
-	};
+  response = {
+    statusCode: 200,
+    body: JSON.stringify(body),
+  };
 
-	// log for debugging
-	//console.info("EVENT\n" + JSON.stringify(event, null, 2));
-	console.info("EVENT\n" + event.body);
-	console.info("BODY\n" + body);
+  // log for debugging
+  //console.info("EVENT\n" + JSON.stringify(event, null, 2));
+  /*
+  console.info("EVENT\n" + event.body);
+  console.info("BODY\n" + body);
+  */
 
-	return response;
+  return response;
 };
+
+// uncomment to test locally with node
+exports.handler();
