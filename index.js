7d59effb8ddda052da7e2e3c4b1b7d3fe59a37dc
Paolo Matchett <paolomatchett@gmail.com>
2021-08-12 10:14:23 -0500

    Add async/await to check for existing customer in Shopify database

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Shopify = require("shopify-api-node");
 const fetch = require("node-fetch");
 
-exports.handler = async (event) => {
-	let itemId;
+exports.handler = async(event) => {
+  let itemId;
 
-	//this is used to connect to a Monday.com Webhook integration and only necessary to run on first setting up
-	// https://support.monday.com/hc/en-us/articles/360003540679-Webhook-Integration-
-	/*
+  //this is used to connect to a Monday.com Webhook integration and only necessary to run on first setting up
+  // https://support.monday.com/hc/en-us/articles/360003540679-Webhook-Integration-
+  /*
   let body = {};
   
 	if (event.body !== null && event.body !== undefined) {
@@ -22,83 +22,116 @@ exports.handler = async (event) => {
   return response;
 	}
   */
+  
 
-	async function createShopifyCustomer(data) {
-		let cleanedCustomer = Object.assign(
-			{},
-			...data.map((obj) => {
-				let reformattedObj = {};
-				if (obj.id == "email") {
-					obj.value = JSON.parse(obj.value).email;
-				} else if (obj.value != null) {
-					obj.value = obj.value.replace(/"/g, "");
-				}
-				reformattedObj[obj.id] = obj.value;
-				return reformattedObj;
-			})
-		);
+  async function createShopifyCustomer(data) {
 
-		let newCustomer = {
-			first_name: cleanedCustomer.first_name,
-			last_name: cleanedCustomer.last_name,
-			email: cleanedCustomer.email,
-			phone: cleanedCustomer.item_id5,
-			verified_email: false,
-			addresses: [
-				{
-					address1: cleanedCustomer.billing_address,
-					address2: cleanedCustomer.bill_addy_unit7,
-					city: cleanedCustomer.bill_addy_city,
-					phone: cleanedCustomer.item_id5,
-					last_name: cleanedCustomer.last_name,
-					first_name: cleanedCustomer.first_name,
-				},
-			],
-			send_email_invite: true,
-			tags: "wholesale",
-			accepts_marketing: true,
-			//Shopify will not create the customer unless the country, province, and zip are validated & correct, so since the Monday form has no validation, we pass it along in the notes
-			note: `Billing Address - State: ${cleanedCustomer.bill_addy_state} Zip: ${cleanedCustomer.bill_addy_zip} Country: ${cleanedCustomer.bill_addy_country5}`,
-		};
+    console.info("DATA PASSED TO CREATE SHOPIFY CUSTOMER\n" + JSON.stringify(data, null, 2));
 
-		const shopify = new Shopify({
-			shopName: process.env.shopName,
-			apiKey: process.env.APIKey,
-			password: process.env.APIPass,
-		});
+    let cleanedCustomer = Object.assign({},
+      ...data.map((obj) => {
+        let reformattedObj = {};
+        if (obj.id == "email") {
+          obj.value = JSON.parse(obj.value).email;
+        }
+        else if (obj.value != null) {
+          obj.value = obj.value.replace(/"/g, "");
+        }
+        reformattedObj[obj.id] = obj.value;
+        return reformattedObj;
+      })
+    );
 
-		await shopify.customer
-			.create(newCustomer)
-			.then((customer) => console.info(customer))
-			.catch((error) => console.info(error));
-	}
+    console.info("CLEANED_CUSTOMER\n" + JSON.stringify(cleanedCustomer, null, 2));
 
-	if (event.body !== null && event.body !== undefined) {
-		let recievedData = JSON.parse(event.body).event;
-		if (recievedData.value.label.text == "APPROVED") {
-			itemId = recievedData.pulseId;
 
-			//construct GraphQL query to get the entire Monday.com item data, since the webhook only sends us one column's value initially
-			let query =
-				"query items($id: Int) { items(ids: [$id] ) { id name column_values { id title value } } }";
+    let newCustomer = {
+      first_name: cleanedCustomer.first_name,
+      last_name: cleanedCustomer.last_name,
+      email: cleanedCustomer.email,
+      phone: cleanedCustomer.item_id5,
+      verified_email: false,
+      addresses: [{
+        address1: cleanedCustomer.billing_address,
+        address2: cleanedCustomer.bill_addy_unit7,
+        city: cleanedCustomer.bill_addy_city,
+        phone: cleanedCustomer.item_id5,
+        last_name: cleanedCustomer.last_name,
+        first_name: cleanedCustomer.first_name,
+      }, ],
+      send_email_invite: true,
+      tags: "wholesale",
+      accepts_marketing: true,
+      //Shopify will not create the customer unless the country, province, and zip are validated & correct, so since the Monday form has no validation, we pass it along in the notes
+      note: `Billing Address - State: ${cleanedCustomer.bill_addy_state} Zip: ${cleanedCustomer.bill_addy_zip} Country: ${cleanedCustomer.bill_addy_country5}`,
+    };
+    console.info("NEW_CUSTOMER\n" + JSON.stringify(newCustomer, null, 2));
 
-			await fetch("https://api.monday.com/v2", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: process.env.MONDAY_API,
-				},
-				body: JSON.stringify({
-					query: query,
-					variables: { id: itemId },
-				}),
-			})
-				.then((r) => r.json())
-				.then((data) =>
-					createShopifyCustomer(data.data.items[0].column_values)
-				);
-		}
-	}
+    const shopify = new Shopify({
+      shopName: process.env.shopName,
+      apiKey: process.env.APIKey,
+      password: process.env.APIPass,
+    });
+    
+   function customerExists(returnedEmail) {
+     return shopify.customer
+      //.search( { email: newCustomer.email })
+      .search( { email: returnedEmail });
+   }
+      
+    /*await (customer) => {
+        console.info(customer) 
+      })
+      .catch((error) => console.info('Shopify API Error: ' + error)); I 
+      */
+    
+    async function createCustomer(newCustomer) {
+    const returnCustomerObj = await customerExists(newCustomer.email);
+      
+      if (returnCustomerObj[0]?.email) {
+        console.info('Customer already exists in Shopify database\n' + JSON.stringify(returnCustomerObj));
+        
+        //add function to tag existing customer
+     
+      } else { 
+      await shopify.customer
+      .create(newCustomer)
+      .then((customer) => console.info(customer))
+      .catch((error) => console.info(error));
+        }
+    }
+    
+    await createCustomer(newCustomer);
+
+  }
+
+  if (event.body !== null && event.body !== undefined) {
+    let recievedData = event;
+
+    if (recievedData.value.label.text == "APPROVED") {
+      itemId = recievedData.pulseId;
+
+      //construct GraphQL query to get the entire Monday.com item data, since the webhook only sends us one column's value initially
+      let query =
+        "query items($id: Int) { items(ids: [$id] ) { id name column_values { id title value } } }";
+
+      await fetch("https://api.monday.com/v2", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: process.env.MONDAY_API,
+          },
+          body: JSON.stringify({
+            query: query,
+            variables: { id: itemId },
+          }),
+        })
+        .then((r) => r.json())
+        .then((data) =>
+          createShopifyCustomer(data.data.items[0].column_values)
+        );
+    }
+  }
 };
 
 // uncomment to test locally with node
